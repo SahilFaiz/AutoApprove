77e8a8b787fdaaa5e379cb5a72032830573207cc
Sayyed Faizan <103510430+SahilFaiz@users.noreply.github.com>
2024-05-08 17:21:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = int(getenv("API_ID", 22163856))
 API_HASH = getenv("API_HASH", "5c7fd7dded5675ce397690fa84d9ed4c")
 BOT_TOKEN = getenv("BOT_TOKEN") #Put your bot token here
 CHANNEL = getenv("CHANNEL", "ZenBotX") #Your public channel username without @ for force subscription.
-MONGO = getenv("MONGO") #Put mongo db url here
+MONGO = getenv("MONGO", "mongodb://mongo:jHGSyGOhBIFqcPLjWJLzFAxzJQiVmSVW@roundhouse.proxy.rlwy.net:36002") #Put mongo db url here
 #Optional Variables
-OWNER_ID = int(getenv("OWNER_ID", 6542673293)) #Go to @ThunderrXbot and type /id and put that value here. 
-FSUB = bool(getenv("FSUB", False)) #Set this True if you want to enable force subscription from users else set to False.
+OWNER_ID = int(getenv("OWNER_ID", 6134208096)) #Go to @ThunderrXbot and type /id and put that value here. 
+FSUB = bool(getenv("FSUB", True)) #Set this True if you want to enable force subscription from users else set to False.
