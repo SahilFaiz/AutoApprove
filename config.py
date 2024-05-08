bc93b794af5940fd082c1cdf5ea13b7d6fb02262
Sayyed Faizan <103510430+SahilFaiz@users.noreply.github.com>
2024-05-08 17:33:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 API_ID = int(getenv("API_ID", 22163856))
 API_HASH = getenv("API_HASH", "5c7fd7dded5675ce397690fa84d9ed4c")
 BOT_TOKEN = getenv("BOT_TOKEN") #Put your bot token here
-CHANNEL = getenv("CHANNEL", "ZenBotX") #Your public channel username without @ for force subscription.
+CHANNEL = getenv("CHANNEL", "webscraperss") #Your public channel username without @ for force subscription.
 MONGO = getenv("MONGO", "mongodb://mongo:jHGSyGOhBIFqcPLjWJLzFAxzJQiVmSVW@roundhouse.proxy.rlwy.net:36002") #Put mongo db url here
 #Optional Variables
 OWNER_ID = int(getenv("OWNER_ID", 6134208096)) #Go to @ThunderrXbot and type /id and put that value here. 
